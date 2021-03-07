7a9c31aa310cd456f1c7a9f611b3106faadabe45
igor036 <igorjoaquim.pg@gmail.com>
2021-03-07 09:57:44 -0300

    create consultarCepAsync method

--- src/main/java/com/linecode/jcep/CorreiosService.java
+++ src/main/java/com/linecode/jcep/CorreiosService.java
@@ -1,5 +1,9 @@
 package com.linecode.jcep;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
+
 import javax.xml.bind.JAXBElement;
 
 import com.linecode.jcep.wsdl.ConsultaCEP;
@@ -7,27 +11,44 @@ import com.linecode.jcep.wsdl.ConsultaCEPResponse;
 import com.linecode.jcep.wsdl.ObjectFactory;
 
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.util.StringUtils;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 public class CorreiosService {
     
+    private static final String EMPTY_CEP_VALUE_ERRO_MESSAGE = "O cep informado como null ou empty string.";
+
+    private static final Pattern VALIDATION_CEP_PATTERN = Pattern.compile("^[0-9]{8}$"); 
     private static final String CORREIOS_SOURCE_CLASS_PACKAGE = "com.linecode.jcep.wsdl";
     private static final String CORREIOS_WEB_SERVICE_API_URI = "https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente";
-
-    private WebServiceTemplate correiosWebServiceTemplate;
+    
+    private final WebServiceTemplate correiosWebServiceTemplate;
 
     public CorreiosService() {
         correiosWebServiceTemplate = buildCorreiosWebServiceTemplate();
     }
+    
+    public CompletableFuture<ConsultaCEPResponse> consultarCepAsync(String cep) {
+        
+        var thread = Executors.newSingleThreadExecutor();
+
+        //@formatter:off
+        return CompletableFuture
+            .supplyAsync(() -> consultarCep(cep), thread)
+            .whenComplete((response, exception) -> thread.shutdown());
+        //@formatter:on
+    }
 
     public ConsultaCEPResponse consultarCep(String cep) {
+
+        assertCep(cep);
 
         var consultaCEP = new ConsultaCEP();
         consultaCEP.setCep(cep);
 
         var request  = new ObjectFactory().createConsultaCEP(consultaCEP);
-        var response = (JAXBElement<ConsultaCEPResponse>) correiosWebServiceTemplate.marshalSendAndReceive(request);
-        
+        var response = this.<ConsultaCEPResponse>callCorreiosWebServiceAction(request);
+    
         return response.getValue();
     }
 
@@ -45,7 +66,22 @@ public class CorreiosService {
 
         var marshaller = new Jaxb2Marshaller();
         marshaller.setPackagesToScan(CORREIOS_SOURCE_CLASS_PACKAGE);
-        
+
         return marshaller;
+    }
+
+    private void assertCep(String cep) {
+        if (!StringUtils.hasText(cep)) {
+            throw new IllegalAccessError(EMPTY_CEP_VALUE_ERRO_MESSAGE);
+        }
+
+        if (!VALIDATION_CEP_PATTERN.matcher(cep).matches()) {
+            throw new IllegalAccessError(EMPTY_CEP_VALUE_ERRO_MESSAGE);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> JAXBElement<T> callCorreiosWebServiceAction(Object request) {
+        return (JAXBElement<T>) correiosWebServiceTemplate.marshalSendAndReceive(request);
     }
 }
--- src/main/java/com/linecode/jcep/JcepApplication.java
+++ src/main/java/com/linecode/jcep/JcepApplication.java
@@ -1,5 +1,7 @@
 package com.linecode.jcep;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,14 +9,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JcepApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		
 		SpringApplication.run(JcepApplication.class, args);
 
 		var correiosService = new CorreiosService();
-		var response = correiosService.consultarCep("49035620");
 
-		System.out.println(response.getReturn().getBairro());
+		correiosService.consultarCepAsync("49035620").whenComplete((endereco, exception) -> {
+
+			if (exception != null) {
+				System.err.println(exception.getMessage());
+			}
+
+			System.out.println(endereco.getReturn().getBairro());
+		});
 	}
 }
 
